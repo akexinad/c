969ae581d195088091ff2c7f0e411ecde6a1a4fb
danny <danixeka@gmail.com>
2019-04-17 11:46:09 +1000

    text editor prep

--- 02-ex1/src/main.c
+++ 02-ex1/src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h> // This is a library inclusion
 
 int main() // main() is the function. The term main has a special meaning in C. Every program should only have ONE main.
+/* multi
+line
+comment */
 {
 	printf("my name is danny\n");
 	return 0;
